85a02dfedc848369af415b98fd5a50a6eefb1596
Caio Encarnação de Queiroz <cencarnacaoq@gmail.com>
2026-03-06 15:56:58 -0300

    feat: add `structlog`as logger

---
+++ app/logging.py
@@ -0,0 +1,33 @@
+import logging
+import structlog
+
+from app.config import settings
+
+
+def configure_logging() -> None:
+    log_level = settings.log_level.upper()
+
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.dev.ConsoleRenderer(),
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(
+        format="%(message)s",
+        level=getattr(logging, log_level, logging.INFO),
+        handlers=[logging.StreamHandler()],
+        force=True,
+    )
